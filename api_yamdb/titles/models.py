0c17687b55e94d282d4fb176a0319eee01ad6bcb
Алексей Гусев <Ankalagon157@yandex.ru>
2025-08-20 22:26:00 +0300

    new branch

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,4 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    post = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True
+    )

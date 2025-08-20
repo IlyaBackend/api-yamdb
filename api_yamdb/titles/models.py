1ebac15c8896bd85e09c564a56c4bf63a0285f46
ArKondr <arkondrashov@yandex.ru>
2025-08-20 22:49:25 +0300

    .

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,4 +1,41 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='Текст поста'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',
+        verbose_name='Группа'
+    )
+    image = models.ImageField(
+        upload_to='posts/',
+        null=True,
+        blank=True,
+        verbose_name='Изображение'
+    )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return (self.text[:STR_LENGTH] + '...'
+                if len(self.text) > STR_LENGTH else self.text)

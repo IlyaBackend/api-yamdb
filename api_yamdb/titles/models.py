53314351589028df7ac8f06872262ac906fefb77
Илья Илья <gamemaster438@yandex.ru>
2025-08-20 22:41:54 +0300

    Test number 100

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -2,3 +2,28 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    post = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(
+        'QWRER добавления', auto_now_add=True
+    )

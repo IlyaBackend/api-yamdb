fafa6b852ca1677aadaed7b5505e40e1871097c8
Алексей Гусев <Ankalagon157@yandex.ru>
2025-08-23 00:36:28 +0300

    добавлены вьюсеты для отзывов и комментариев, прописан рейтинг для произведений

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -9,7 +9,25 @@ User = get_user_model()
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    pass
+    """Сериализатор для произведений."""
+    average_rating = serializers.SerializerMethodField(read_only=True)
+    reviews_number = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = [
+            'id', 'name', 'year', 'description', 'category', 'genre',
+            'average_rating', 'reviews_rating'
+        ]
+
+    def get_average_rating(self, title):
+        """
+        Возвращает среднюю оценку произведения
+        или сообщение об отсутствии отзывов.
+        """
+        avg_rating = getattr(title, 'average_rating', None)
+        return round(avg_rating) if avg_rating is not None else (
+            f'Произведение {title.name} еще не имеет отзывов.')
 
 
 class ReviewSerializer(serializers.ModelSerializer):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.db.models import Count, Avg
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+from api.serializers import (
+    CommentSerializer, ReviewSerializer, TitleSerializer
+)
+from titles.models import Review, Title
+from users.permissions import IsAdmin
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Класс для управления отзывов на произведения."""
+    serializer_class = TitleSerializer
+
+    def get_title_info(self):
+        return Title.objects.annotate(
+            average_rating=Avg('reviews__rating'),
+            reviews_number=Count('reviews'),
+        ).order_by('-average_rating')
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Класс для управления отзывов на произведения."""
+    serializer_class = ReviewSerializer
+    lookup_field = 'id'
+    permission_classes = [IsAdmin, IsAuthenticatedOrReadOnly]
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs['title_id'])
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_title())
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Класс для управления комментариев к отзывам."""
+    serializer_class = CommentSerializer
+    lookup_field = 'id'
+    permission_classes = [IsAdmin, IsAuthenticatedOrReadOnly]
+
+    def get_review(self):
+        return get_object_or_404(Review, id=self.kwargs['review_id'])
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_review())

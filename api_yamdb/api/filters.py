d960d5c375496205badeaa4134aa4d249a85efd4
ArKondr <arkondrashov@yandex.ru>
2025-08-24 16:33:50 +0300

    Исправленные ошибки в тестах

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,13 @@
+from django_filters import CharFilter, FilterSet, NumberFilter
+from reviews.models import Title
+
+
+class TitleFilters(FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='icontains')
+    year = NumberFilter(field_name='year',)
+    category = CharFilter(field_name='category__slug', )
+    genre = CharFilter(field_name='genre__slug', )
+
+    class Meta:
+        model = Title
+        fields = ('name', 'year', 'category', 'genre',)
